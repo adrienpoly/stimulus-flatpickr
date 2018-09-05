b60a0418614508d61b10dcb1f8e4249a2e48f42f
Adrien Poly <adrienpoly@gmail.com>
2018-09-05 11:07:20 +0200

    fix issue for time_24hr

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 export const kebabCase = string =>
   string
     .replace(/([a-z])([A-Z])/g, "$1-$2")
-    .replace(/\s+/g, "-")
+    .replace(/[\s_]+/g, "-")
     .toLowerCase();
 
 export const capitalize = string => {
