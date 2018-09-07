fed35087cdc0fa3ad22db7252a6f402be6513225
Adrien Poly <adrienpoly@gmail.com>
2018-09-07 16:52:28 +0200

    ads coverage reporting

--- karma.conf.js
+++ karma.conf.js
@@ -26,7 +26,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      "spec/*.js": ["webpack", "sourcemap"],
+      "spec/*.js": ["webpack", "sourcemap", "coverage"],
       "spec/fixtures/*.html": ["html2js"]
     },
 
@@ -56,8 +56,11 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ["mocha"],
+    reporters: ["mocha", "coverage"],
 
+    coverageReporter: {
+      reporters: [{ type: "lcov" }]
+    },
     // web server port
     port: 9876,
 
